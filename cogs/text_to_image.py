a67576bf48563bbff160500e5c69db918794ceb6
DELL <nqdior@gmail.com>
2024-02-06 01:37:58 +0900

    基本的な動きは実装完了

--- cogs/text_to_image.py
+++ cogs/text_to_image.py
@@ -13,48 +13,11 @@ API_KEY = os.getenv("STABILITY_API_KEY")
 if not API_KEY:
     raise EnvironmentError("Missing Stability API key.")
 
-class ImageGenerationOptions:
-    def __init__(self, 
-                 prompt: str,
-                 negative_prompt: str = "",
-                 cfg_scale: float = 7.0,
-                 clip_guidance_preset: str = "NONE",
-                 aspect: str = "square 1:1",
-                 style: str = "None",
-                 sampler: str = None,
-                 seed: int = None,
-                 model: str = "Stable Diffusion XL 1.0"):
-        self.prompt = prompt
-        self.negative_prompt = negative_prompt
-        self.cfg_scale = cfg_scale
-        self.clip_guidance_preset = clip_guidance_preset
-        self.aspect = aspect
-        self.style = style
-        self.sampler = sampler
-        self.seed = seed
-        self.model = model
-
-    def to_dict(self):
-        return {
-            "prompt": self.prompt,
-            "negative_prompt": self.negative_prompt,
-            "cfg_scale": self.cfg_scale,
-            "clip_guidance_preset": self.clip_guidance_preset,
-            "aspect": self.aspect,
-            "style": self.style,
-            "sampler": self.sampler,
-            "seed": self.seed,
-            "model": self.model
-        }
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(**data)
-
 class IMAGINE(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Imagineコマンドの実装
     @slash_command(name="imagine", description="Stable Diffusionを使用して画像を生成します。")
     async def imagine(self, ctx, 
                       prompt: Option(str, "画像生成に使用するプロンプトを入力してください。(英語のみ)", required=True),
@@ -67,8 +30,11 @@ class IMAGINE(commands.Cog):
                       seed: Option(int, "ランダムノイズシードを0～4294967295の範囲で入力してください。空欄の場合は自動採番されます。", required=False, min_value=0, max_value=4294967295),
                       model: Option(str, "画像生成に使用するStable Diffusionモデルの種類を、選択肢から選んでください。", choices=list(model_options.keys()), required=False, default="Stable Diffusion XL 1.0")
                       ):
-                                          
+        
+        # ユーザーの入力を検証                  
         await ctx.defer()
+
+        # 画像生成のリクエストJSONを作成
         width, height = aspect_ratio_options[aspect]
         json_data = {
             "text_prompts": [{"text": prompt, "weight": 1}] + ([{"text": negative_prompt, "weight": -1}] if negative_prompt else []),
@@ -82,37 +48,58 @@ class IMAGINE(commands.Cog):
             **({"style_preset": style_preset_options[style]} if style != "None" else {}),
             **({"seed": seed} if seed else {}),
         }
+
+        # 画像生成のリクエストを送信
         response = requests.post(f"{API_HOST}/v1/generation/{model_options[model]}/text-to-image", headers={
             "Authorization": f"Bearer {API_KEY}",
             "Content-Type": "application/json"
         }, json=json_data)
 
+        # 画像生成のリクエストが成功したかどうかを検証
         if response.status_code != 200:
-            await ctx.respond(embed=discord.Embed(color=discord.Color.red(), description="Error: " + response.text))
-            return
+            embed=discord.Embed(
+                color=discord.Color.red(), 
+                description="不適切なプロンプトが検出されたため、実行を中断しました。",
+            )
+            embed.set_footer(text=f"プロンプトは英語で入力してください。\nまた、NSFWコンテンツを生成することは禁止されています。")
 
-        data = response.json()
+            # エラーの場合、エラーメッセージを送信して終了
+            await ctx.respond(embed=embed)
+            return
+        
+        # 画像生成の結果を取得
         files = []
         seeds = ""
-        for i, image in enumerate(data.get("artifacts", [])):
+        filterd = 0
+        for i, image in enumerate(response.json().get("artifacts", [])):
+            # NSFWコンテンツが検出された場合、画像を除外
+            if "SUCCESS" != image.get("finishReason") :
+                filterd += 1
+                continue
             files.append(discord.File(io.BytesIO(base64.b64decode(image["base64"])), filename=f"image{i}.png"))
             seeds += f"  - image{i+1}: `{image['seed']}`\n"
+        
+        # 画像生成に使用された情報を構成
         embed = discord.Embed(
-            # 投稿者のアイコンを表示
-                    description=
-                        f"{ctx.author.mention}'s Imagine\n\n" +
-                        f"- model: `{model}`\n" +
-                        f"- prompt: `{prompt}`\n" +
-                        (f"- negative: `{negative_prompt}`\n" if negative_prompt else "") +
-                        f"- aspect: `{aspect}({width}:{height})`\n" +
-                        (f"- cfg_scale: `{cfg_scale}`\n" if cfg_scale else "") +
-                        (f"- clip_guidance_preset: `{clip_guidance_preset}`\n" if clip_guidance_preset != "NONE" else "") +
-                        (f"- sampler: `{sampler}`\n" if sampler else "") +
-                        (f"- style_preset: `{style}`\n" if style != "None" else "") +
-                        f"- seed:\n{seeds}\n",
-                    color=discord.Color.blurple() 
-                )
-        embed.set_author(name=f"{ctx.author.name}" ,icon_url=ctx.author.avatar.url)
+                description=
+                    f"**{ctx.author.mention}'s Imagine**\n\n" +
+                    f"- model: `{model}`\n" +
+                    f"- prompt: `{prompt}`\n" +
+                    (f"- negative: `{negative_prompt}`\n" if negative_prompt else "") +
+                    f"- aspect: `{aspect}({width}:{height})`\n" +
+                    (f"- cfg_scale: `{cfg_scale}`\n" if cfg_scale else "") +
+                    (f"- clip_guidance_preset: `{clip_guidance_preset}`\n" if clip_guidance_preset != "NONE" else "") +
+                    (f"- sampler: `{sampler}`\n" if sampler else "") +
+                    (f"- style_preset: `{style}`\n" if style != "None" else "") +
+                    f"- seed:\n{seeds}" +
+                    # NSFWコンテンツが存在した場合、検出結果を追記
+                    (f"\n\nNSFWコンテンツが検出されたため、{filterd}枚の画像が除外されています。" if filterd !=0 else ""),
+                color=discord.Color.blurple() 
+            )
+        
+        # 画像生成の結果を送信
+        embed.set_thumbnail(url="https://images.squarespace-cdn.com/content/v1/646b4513dbebfb2c0adc2b52/1684751740466-5T0GG85P7CVA04C4SKKB/StabilityAi_Logo_White-19.png")
+        embed.set_footer(text=f"created by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
         await ctx.respond(embed=embed, files=files)
 
 def setup(bot):
