f925e44e041847bce7824fa1534f12d76387e9fc
DELL <nqdior@gmail.com>
2024-02-06 03:09:02 +0900

    アップスケールまで実装完了

--- cogs/text_to_image.py
+++ cogs/text_to_image.py
@@ -1,6 +1,7 @@
 import discord
 import requests
-import os, io, base64, random
+import os, io, base64
+from discord.ui import Button, View
 from discord.ext import commands
 from discord.commands import slash_command, Option
 from dotenv import load_dotenv
@@ -17,7 +18,7 @@ class IMAGINE(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Imagineコマンドの実装
+    ### Imagineコマンドの実装
     @slash_command(name="imagine", description="Stable Diffusionを使用して画像を生成します。")
     async def imagine(self, ctx, 
                       prompt: Option(str, "画像生成に使用するプロンプトを入力してください。(英語のみ)", required=True),
@@ -41,7 +42,7 @@ class IMAGINE(commands.Cog):
             "height": height,
             "width": width,
             "samples": 4,
-            "steps": 40,
+            "steps": 10,
             **({"cfg_scale": cfg_scale} if style != "None" else {}),
             **({"clip_guidance_preset": clip_guidance_preset}),
             **({"sampler": sampler} if sampler != "None" else {}),
@@ -70,14 +71,20 @@ class IMAGINE(commands.Cog):
         # 画像生成の結果を取得
         files = []
         seeds = ""
-        filterd = 0
+        nsfw_content_count = 0
+        view = discord.ui.View()
         for i, image in enumerate(response.json().get("artifacts", [])):
+            
             # NSFWコンテンツが検出された場合、画像を除外
             if "SUCCESS" != image.get("finishReason") :
-                filterd += 1
+                nsfw_content_count += 1
                 continue
+
             files.append(discord.File(io.BytesIO(base64.b64decode(image["base64"])), filename=f"image{i}.png"))
             seeds += f"  - image{i+1}: `{image['seed']}`\n"
+
+            button = discord.ui.Button(label=f"upscale {i+1}", custom_id=f"{i+1}")
+            view.add_item(button)
         
         # 画像生成に使用された情報を構成
         embed = discord.Embed(
@@ -93,14 +100,74 @@ class IMAGINE(commands.Cog):
                     (f"- style_preset: `{style}`\n" if style != "None" else "") +
                     f"- seed:\n{seeds}" +
                     # NSFWコンテンツが存在した場合、検出結果を追記
-                    (f"\n\nNSFWコンテンツが検出されたため、{filterd}枚の画像が除外されています。" if filterd !=0 else ""),
+                    (f"\n\nNSFWコンテンツが検出されたため、{nsfw_content_count}枚の画像が除外されています。" if nsfw_content_count !=0 else ""),
                 color=discord.Color.blurple() 
             )
-        
+            
         # 画像生成の結果を送信
         embed.set_thumbnail(url="https://images.squarespace-cdn.com/content/v1/646b4513dbebfb2c0adc2b52/1684751740466-5T0GG85P7CVA04C4SKKB/StabilityAi_Logo_White-19.png")
         embed.set_footer(text=f"created by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
-        await ctx.respond(embed=embed, files=files)
+        await ctx.respond(embed=embed, files=files, view=view)
+
+
+        ### upscalerボタンのコールバックを実装
+        async def button_callback(interaction: discord.Interaction):
+            await interaction.response.defer()
+
+            # ボタンが押されたときの処理
+            original_message = interaction.message
+            custom_id = int(interaction.data["custom_id"]) - 1
+
+            # 画像をダウンロードしてバイナリデータを取得
+            attachment_url = original_message.attachments[custom_id].url
+            response = requests.get(attachment_url)
+
+            # 画像のダウンロード失敗時のエラー処理
+            if response.status_code != 200:
+                embed=discord.Embed(
+                    color=discord.Color.red(), 
+                    description="システムエラーが発生したため、実行を中断しました。",
+                )
+                embed.set_footer(text=f"時間を置いて再度お試しください。")
+                await interaction.followup.send(embed=embed)
+                return
+            
+            # 画像のダウンロード成功時の処理
+            image_data = response.content
+
+            # 画像をアップスケール
+            engine_id = "esrgan-v1-x2plus" # esrgan-v1-x2plus
+            response = requests.post(
+                f"{API_HOST}/v1/generation/{engine_id}/image-to-image/upscale",
+                headers={
+                    "Accept": "application/json",
+                    "Authorization": f"Bearer {API_KEY}"
+                },
+                files={
+                    "image": image_data
+                }
+            )
+            # アップスケールの結果を解析
+            files = []
+            for i, image in enumerate(response.json().get("artifacts", [])):
+                files.append(discord.File(io.BytesIO(base64.b64decode(image["base64"])), filename=f"image{i}.png"))
+
+            # 画像生成に使用された情報を構成
+            embed = discord.Embed(
+                description=
+                    f"**{ctx.author.mention}'s UpScale**\n\n" +
+                    f"- model: `{engine_id}`\n",
+                color=discord.Color.blurple() 
+            )
+            
+            # 画像生成の結果を送信
+            embed.set_thumbnail(url="https://images.squarespace-cdn.com/content/v1/646b4513dbebfb2c0adc2b52/1684751740466-5T0GG85P7CVA04C4SKKB/StabilityAi_Logo_White-19.png")
+            embed.set_footer(text=f"created by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
+            await interaction.followup.send(embed=embed, files=files)
+
+        for item in view.children:
+            if isinstance(item, Button):
+                item.callback = button_callback
 
 def setup(bot):
     bot.add_cog(IMAGINE(bot))
